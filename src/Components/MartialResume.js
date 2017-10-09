660b3b66cb86c1b168000dac581638f7eb869d18
[A[A[B[B[B[Rikki Klein <shayneeG1>
2017-10-09 00:01:03 -0400

    to master

--- src/Components/MartialResume.js
+++ src/Components/MartialResume.js
@@ -8,48 +8,141 @@ class MartialResume extends Component {
       <div className="res-type">SENSEI DAVID KLEIN </div>
       <div className="res-title">EDUCATION</div>
       <div className="res-info">
-      Tora Dojo – First Degree Black Belt Brooklyn, New York <br/>
-        •	Shotokan based Martial Art (8 Years) <br/>
-        •	Studied under Masters Jeffery Glanz and Jack Newman <br/>
-        •	Perfected stances, various kicks and hand techniques, kata, and sparring <br/>
-      Systema	 Jerusalem, Israel <br/>
-        •	Russian Martial Art (3 Years) <br/>
-        •	Learned hand-to-hand combat, grappling, knife fighting <br/>
-        •	Gained knowledge of breathing, relaxation, and fluidity of movement while in a pressurized environment <br/>
-      MCMAP Jerusalem, Israel <br/>
-        •	Marine Corps Martial Arts Program  (1 Year) <br/>
-        •	Studied under Recon Marine David Stern <br/>
-        •	Focused on hand-to-hand and close quarters combat techniques <br/>
-        Boxing	Brooklyn, New York <br/>
-          •	Studied at the Underground Boxing Gym under Ilia Mesishchev (3 months) <br/>
-          •	Focused on agility, footwork, and punching power <br/>
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">Tora Dojo</span>
+            – First Degree Black Belt
+            </span>
+          <span className="res-date">
+            Brooklyn, New York
+          </span>
         </div>
-        <div className="res-title">EXPERIENCE</div>
-        <div className="res-info">
-          Boro Park YM-YWHA Brooklyn, New York <br/>
-          Lead Martial Arts Instructor September 2013-Present <br/>
-          •	Guide and develop students’ (children & adults) discipline, focus, confidence, and ability to protect oneself <br/>
-          •	Teaching emphasis is placed on Systema with a mixture of other arts <br/>
-          •	Lead military-style battlefield simulation exercises <br/>
-          •	Lead Insanity Cardio Fitness class <br/>
-          Bring The Gym To Me                                                                                                                              Brooklyn, New York
-Martial Arts Instructor                                                                                                                     September 2016-June 2017
-•	Program geared for children with ADD
-•	Taught self-defense techniques and movements
+        <ul>
+          <li>Shotokan based Martial Art (8 Years)</li>
+          <li>Studied under Masters Jeffery Glanz and Jack Newman</li>
+          <li>	Perfected stances, various kicks and hand techniques, kata, and sparring</li>
+        </ul>
+      </div>
 
-YALA                                                                                                                                                       Brooklyn, New York
-Martial Arts Instructor                                                                                                                 September 2013-August 2013
-•	Program geared for children with special needs
-•	Taught Tora Dojo to 8 children
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">Systema</span>
+          </span>
+          <span className="res-date">
+            Jerusalem, Israel
+          </span>
+        </div>
+        <ul>
+          <li>Russian Martial Art (3 Years)</li>
+          <li>Learned hand-to-hand combat, grappling, knife fighting</li>
+          <li>Gained knowledge of breathing, relaxation, and fluidity of movement while in a pressurized environment</li>
+        </ul>
+      </div>
 
-Camp Nageela Midwest                                                                                                                                 Marshall, Indiana
-Martial Arts Instructor                                                                                                                               Summers 2009-2016
-•	Taught Tora Dojo
-•	Led military-style battlefield simulation exercises
-•	Led Insanity Cardio Fitness classes
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">MCMAP</span>
+          </span>
+          <span className="res-date">
+            Jerusalem, Israel
+          </span>
+        </div>
+        <ul>
+          <li>Marine Corps Martial Arts Program  (1 Year)</li>
+          <li>Studied under Recon Marine David Stern</li>
+          <li>Focused on hand-to-hand and close quarters combat techniques</li>
+        </ul>
+      </div>
 
-</div>
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">Boxing</span>
+          </span>
+          <span className="res-date">
+            Brooklyn, New York
+          </span>
+        </div>
+        <ul>
+          <li>Studied at the Underground Boxing Gym under Ilia Mesishchev (3 months)</li>
+          <li>Focused on agility, footwork, and punching power</li>
+        </ul>
+      </div>
 
+      <div className="res-title">EXPERIENCE</div>
+
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">Boro Park YM-YWHA</span> <br/>
+            Lead Martial Arts Instructor
+          </span>
+          <span className="res-date">
+            Brooklyn, New York <br/>
+            September 2013-Present
+          </span>
+        </div>
+        <ul>
+          <li>Guide and develop students’ (children & adults) discipline, focus, confidence, and ability to protect oneself</li>
+          <li>Teaching emphasis is placed on Systema with a mixture of other arts</li>
+          <li>Lead military-style battlefield simulation exercises </li>
+          <li>Lead Insanity Cardio Fitness class</li>
+        </ul>
+      </div>
+
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">Bring The Gym To Me</span> <br/>
+            Martial Arts Instructor
+          </span>
+          <span className="res-date">
+            Brooklyn, New York <br/>
+            September 2016-June 2017
+          </span>
+        </div>
+        <ul>
+          <li>Program geared for children with ADD</li>
+          <li>Taught self-defense techniques and movements</li>
+        </ul>
+      </div>
+
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">YALA </span> <br/>
+            Martial Arts Instructor
+          </span>
+          <span className="res-date">
+            Brooklyn, New York <br/>
+            September 2013-August 2013
+          </span>
+        </div>
+        <ul>
+          <li>Program geared for children with special needs</li>
+          <li>Taught Tora Dojo to 8 children</li>
+        </ul>
+      </div>
+
+      <div className="res-info">
+        <div className="res-info-title">
+          <span>
+            <span className="res-bold">Camp Nageela Midwest</span> <br/>
+            Martial Arts Instructor
+          </span>
+          <span className="res-date">
+            Marshall, Indiana <br/>
+            Summers 2009-2016
+          </span>
+        </div>
+        <ul>
+          <li>Taught Tora Dojo</li>
+          <li>Led military-style battlefield simulation exercises</li>
+          <li>Led Insanity Cardio Fitness classes</li>
+        </ul>
+      </div>
       </section>
     );
   }
--- src/Components/MathResume.js
+++ src/Components/MathResume.js
@@ -5,7 +5,107 @@ class MathResume extends Component {
   render() {
     return (
       <section id="projects" className="resume">
-        <div className="title">Projects</div>
+        <div className="res-type">MATH RESUME HERE!!!</div>
+        <div className="res-title">EDUCATION</div>
+        <div className="res-info">
+          <div className="res-info-title">
+            <span>
+              <span className="res-bold">NYU Tandon School of Engineering</span><br/>
+              Bachelor of Science in Civil Engineering
+            </span>
+            <span className="res-date">
+              Brooklyn, New York <br/>
+              January 2013-December 2016
+            </span>
+          </div>
+          <div className="res-degree">
+            Minor: Construction Management <br/>
+            GPA: 3.27 <br/>
+            Passed FE Exam: January 25, 2017
+          </div>
+        </div>
+        <div className="res-title">RELEVANT COURSES & PROJECTS</div>
+        <div className="res-info">
+          <div className="res-bold">Leonardo Da Vinci’s Swing Bridge Project: </div>
+          <ul>
+            <li>Design of a 15th century wooden swing bridge</li>
+            <li>Conducted analysis and design of the bridge using FEA software </li>
+          </ul>
+          <div className="res-bold">Timber and Masonry Design: </div>
+          <ul>
+            <li>Design of a two-story office building comprised of timber and masonry</li>
+            <li>Conducted analysis and design of joists, girders, columns, walls, lintels, and connections</li>
+          </ul>
+          <div className="res-bold">Senior Civil Design Project:</div>
+          <ul>
+            <li>Voted Design Team Leader by peers</li>
+            <li>Coordinated with the project CM to design and prepare a complete site development package</li>
+            <li>Grading, drainage, stormwater management, wastewater/water supply, street design, sediment & erosion control</li>
+          </ul>
+          <div className="res-bold">Senior Structural Design Project:</div>
+          <ul>
+            <li>Developed a complete structural framing system for a16-story reinforced concrete apartment building</li>
+            <li>Conducted analysis and design of the system using FEA software</li>
+            <li>Prepared engineering design computations, construction drawings, and specifications</li>
+          </ul>
+          <div className="res-bold">OSHA 10 Hour Construction: OEC-11617657</div>
+        </div>
+        <div className="res-title">EXPERIENCE</div>
+        <div className="res-info">
+          <div className="res-info-title">
+            <span>
+              <span className="res-bold">Mega Contracting Group</span><br/>
+              Assistant Project Manager
+            </span>
+            <span className="res-date">
+              Astoria, New York<br/>
+              May 2017-Present
+            </span>
+          </div>
+          <ul>
+            <li>Handles all submittals, RFI’s, change orders, purchase orders, and meeting minutes </li>
+            <li>Assists in creating project schedule, and take-offs for all project material </li>
+            <li>Maintains coordination between trades, architect, engineer, and owners </li>
+          </ul>
+
+          <div className="res-info-title">
+            <span>
+              <span className="res-bold"> Yaakov Stern, P.E., Consulting Engineers</span><br/>
+              Structural Engineer Intern
+            </span>
+            <span className="res-date">
+              Brooklyn, New York<br/>
+              Summer 2015
+            </span>
+          </div>
+          <ul>
+            <li>Drafted structural plans, sections and details</li>
+            <li>Reviewed calculations and plans at various levels of development</li>
+          </ul>
+        </div>
+        <div className="res-title">TECHNICAL SKILLS</div>
+          <div className="res-info">
+            Microsoft Office, AutoCAD, Revit, ETABS, SAP, SAGE 300, Primavera P6, SAFE
+          </div>
+          <div className="res-title">ACTIVITIES & HONORS</div>
+          <div className="res-info">
+          <div className="res-info-title">
+            <span>
+              <span className="res-bold">Chi Epsilon </span> <br/>(National Civil Engineering Honor Society) - Tutor
+            </span>
+            <span className="res-date">
+              December 2015-Present
+            </span>
+          </div>
+          <div className="res-info-title">
+            <span>
+              <span className="res-bold">Tau Beta Pi </span> <br/>(National Engineering Honor Society) - Project Creator/Leader -“Tandon: In The Know”
+            </span>
+            <span className="res-date">
+              April 2015-Present
+            </span>
+          </div>
+        </div>
       </section>
     );
   }
